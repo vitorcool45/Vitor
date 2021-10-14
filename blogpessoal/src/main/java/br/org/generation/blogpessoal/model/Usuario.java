58fdde2a386451c562e6391fe638cdf47ad33d63
unknown <vitor.santana03@hotmail.com>
2021-10-14 14:44:15 -0300

    Deploy do BlogPessoal finalizado

--- blogpessoal/src/main/java/br/org/generation/blogpessoal/model/Usuario.java
+++ blogpessoal/src/main/java/br/org/generation/blogpessoal/model/Usuario.java
@@ -15,6 +15,8 @@ import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import io.swagger.annotations.ApiModelProperty;
+
 @Entity
 @Table(name = "tb_usuario")
 public class Usuario {
@@ -27,6 +29,7 @@ public class Usuario {
 	@Size (min = 2, max = 100)
 	private String nome;
 	
+	@ApiModelProperty(example = "email@email.com.br")
 	@NotNull
 	@Size (min = 5, max = 100)
 	@Email
